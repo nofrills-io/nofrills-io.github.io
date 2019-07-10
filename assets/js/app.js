5553cb808f8fdaf0fb5e2332ef3987c58a49d314
Cezary Stępkowski <cezary.stepkowski@wingu.de>
2019-07-10 20:30:04 +0200

    Updated website

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,11 @@ require('bootstrap');
 $(document).ready(function () {
     $(".navbar-open").click(function () {
         $("#main-navbar").fadeIn();
+        $("body").addClass('modal-open');
     });
 
     $(".navbar-close").click(function () {
         $("#main-navbar").fadeOut();
+        $("body").removeClass('modal-open');
     });
 });
